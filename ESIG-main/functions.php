db830c128b5effb462c4b2624843696b98684e4f
madiou <mahdiou.diallo@gmail.com>
2026-01-21 21:43:25 -0500

    Refactor asset enqueueing in functions.php

--- ESIG-main/functions.php
+++ ESIG-main/functions.php
@@ -1,6 +1,20 @@
 <?php
 function esig_assets() {
-  wp_enqueue_style('esig-css', get_stylesheet_directory_uri() . '/css/style.css', array(), '1.0');
-  wp_enqueue_script('esig-js', get_stylesheet_directory_uri() . '/js/main.js', array(), '1.0', true);
+  // CSS principal
+  wp_enqueue_style(
+    'esig-css',
+    get_stylesheet_directory_uri() . '/css/style.css',
+    array(),
+    '1.0'
+  );
+
+  // JS principal
+  wp_enqueue_script(
+    'esig-js',
+    get_stylesheet_directory_uri() . '/js/main.js',
+    array(),
+    '1.0',
+    true
+  );
 }
 add_action('wp_enqueue_scripts', 'esig_assets');
